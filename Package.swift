e733998ed7ae347aa1e1b1f7549058fe8187c792
Scott Yelvington <scott@classkick.com>
2025-08-12 16:00:33 -0700

    Adds SPM support

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "UIImagePDF",
+    platforms: [
+        .iOS(.v12)
+    ],
+    products: [
+        .library(name: "UIImagePDF", targets: ["UIImagePDF"])
+    ],
+    targets: [
+        .target(
+            name: "UIImagePDF",
+            path: "UIImage+PDF",
+            publicHeadersPath: ".",
+            cSettings: [
+                .headerSearchPath(".")
+            ]
+        )
+    ]
+)
